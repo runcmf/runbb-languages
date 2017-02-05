04edb226351a09afb30a635527a1ed2c5c642b75
1f7 <1f7.wizard@gmail.com>
2017-02-05 05:45:26 +0300

    add info

---
+++ info.php
@@ -0,0 +1,18 @@
+<?php return [
+    0 => [
+        'code' => 'en',
+        'locale' => 'en_US',
+        'name' => 'English',
+        'version' => '1.0.0',
+        'image' => 'English.svg',
+        'author' => '',
+    ],
+    1 => [
+        'code' => 'ru',
+        'locale' => 'ru_RU',
+        'name' => 'Russian',
+        'version' => '1.0.1',
+        'image' => 'Russian.svg',
+        'author' => '',
+    ]
+];
